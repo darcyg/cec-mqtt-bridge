08687033d45f4c88e49d5ddcd5793ce4a288f141
Garrett L. Ward <glward@glward.net>
2018-01-06 11:51:02 -0500

    Add TV State object and tracking

--- src/main.c
+++ src/main.c
@@ -9,12 +9,18 @@
 #include "ceccloader.h"
 
 
+typedef struct tv_state {
+    cec_power_status power_status;
+    uint8_t hdmi_input;
+} TvState;
+
+
 static bool terminate  = false;
+static bool debug = false;
 
-
-static void cb_cec_configuration_changed(void *lib, const libcec_configuration* configuration);
 static void cb_cec_command_received(void *lib, const cec_command* command);
 static void cb_cec_log_message(void *lib, const cec_log_message* message);
+static bool tv_state_equal(TvState *a, TvState *b);
 
 static char devices[16][16] = {
     "TV",
@@ -39,26 +45,42 @@ static void sighandler(int signal);
 
 
 // Set up callbacks
-static ICECCallbacks        g_callbacks = {
+static ICECCallbacks        callbacks = {
     .logMessage           = NULL,
     .keyPress             = NULL,
     .commandReceived      = cb_cec_command_received,
-    .configurationChanged = cb_cec_configuration_changed,
+    .configurationChanged = NULL,
     .alert                = NULL,
     .menuStateChanged     = NULL,
     .sourceActivated      = NULL
 };
 
+static TvState tv_state = {
+    .power_status = CEC_POWER_STATUS_UNKNOWN,
+    .hdmi_input = 0
+};
 
-
-static void cb_cec_configuration_changed(void *lib, const libcec_configuration* configuration)
+static void cb_cec_command_received(void *p, const cec_command* command)
 {
-    printf("%s\n", configuration->strDeviceName);
-}
-
-static void cb_cec_command_received(void *lib, const cec_command* command)
-{
+    (void)p;
     char buffer[24];
+
+    TvState old = tv_state;
+
+    if (debug)
+    {
+        libcec_opcode_to_string(command->opcode, buffer, sizeof(buffer));
+        printf("%s -> %s: %s ( ", devices[command->initiator], devices[command->destination], buffer);
+        if (command->parameters.size > 0)
+        {
+            for (int i = 0; i < command->parameters.size; i++)
+            {
+                printf("%02x ", command->parameters.data[i]);
+            }
+        }
+        printf(")\n");
+    }
+    
 
     if (command->initiator == CECDEVICE_TV)
     {
@@ -69,33 +91,74 @@ static void cb_cec_command_received(void *lib, const cec_command* command)
         switch (command->opcode)
         {
         case CEC_OPCODE_REPORT_POWER_STATUS:
-            libcec_power_status_to_string(command->parameters.data[0], buffer, sizeof(buffer));
-            printf("TV State: %s\n", buffer);
+            tv_state.power_status = command->parameters.data[0];
             break;
         case CEC_OPCODE_VENDOR_COMMAND_WITH_ID:
         case CEC_OPCODE_STANDBY:
-            printf("TV State: standby\n");
+            // Bravia TV broadcasts a vendor command on any
+            // on->standby transition
+            tv_state.power_status = CEC_POWER_STATUS_STANDBY;
             break;
         case CEC_OPCODE_REPORT_PHYSICAL_ADDRESS:
-            printf("TV State: on\n");
-            // printf("TV State: ??? (");
-            // for (int i = 0; i < command->parameters.size; i++)
-            // {
-            //     printf("%02x ", command->parameters.data[i]);
-            // }
-            // printf(")\n");
+            // Bravia TV broadcasts its physical address on any
+            // standby->on transition
+            tv_state.power_status = CEC_POWER_STATUS_ON;
+            break;
+        case CEC_OPCODE_SET_STREAM_PATH: // from tv to bcast
+            tv_state.hdmi_input = command->parameters.data[0] >> 4;
             break;
         default:
             break;
         }
     }
+    else if (command->destination == CECDEVICE_BROADCAST)
+    {
+        switch (command->opcode)
+        {
+        case CEC_OPCODE_ACTIVE_SOURCE: // from src to bcast
+            tv_state.hdmi_input = command->parameters.data[0] >> 4;
+            break;
+        default:
+            break;
+        }
+    }
+
+    if (!tv_state_equal(&old, &tv_state))
+    {
+        char oldpower[24];
+        char newpower[24];
+        libcec_power_status_to_string(old.power_status, oldpower, sizeof(oldpower));
+        libcec_power_status_to_string(tv_state.power_status, newpower, sizeof(newpower));
+        printf("TV State: (%s [HDMI %d]) -> (%s [HDMI %d])\n",
+               oldpower, old.hdmi_input,
+               newpower, tv_state.hdmi_input
+        );
+    }
 }
 
-static void cb_cec_log_message(void *lib, const cec_log_message* message)
+static void cb_cec_log_message(void *p, const cec_log_message* message)
 {
+    (void)p;
     printf("%s\n", message->message);
 }
 
+
+static bool tv_state_equal(TvState *a, TvState *b)
+{
+    // if non-NULL and reference equal, true
+    if (a != NULL && a == b)
+    {
+        return true;
+    }
+    // If both NULL or a OR b is NULL, false
+    if (!a || !b)
+    {
+        return false;
+    }
+    
+    return a->power_status == b->power_status
+        && a->hdmi_input == b->hdmi_input;
+}
 
 static void sighandler(int signal)
 {
@@ -107,6 +170,9 @@ static void sighandler(int signal)
 
 int main(int argc, char *argv[])
 {
+    // TODO command line arguments for adapter and MQTT
+    (void)argc;
+    (void)argv;
     if (signal(SIGINT, sighandler) == SIG_ERR)
     {
         printf("can't register sighandler\n");
@@ -120,7 +186,7 @@ int main(int argc, char *argv[])
     libcecc_reset_configuration(&config);
     config.clientVersion = LIBCEC_VERSION_CURRENT;
     config.bActivateSource = 0;
-    config.callbacks = &g_callbacks;
+    config.callbacks = &callbacks;
     config.deviceTypes.types[0] = CEC_DEVICE_TYPE_RECORDING_DEVICE;
     snprintf(config.strDeviceName, sizeof(config.strDeviceName), "cec-mqtt");
 
@@ -132,7 +198,6 @@ int main(int argc, char *argv[])
     }
 
     // Assume RPI for now
-
     if (!iface.open(iface.connection, "RPI", 5000))
     {
         fprintf(stderr, "Unable to open device on port RPI\n");
@@ -140,11 +205,11 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
-
 //    printf("Opened RPI device\n");
 
     while (!terminate)
     {
+        // Nothing to do
         sleep(1);
     }
     
